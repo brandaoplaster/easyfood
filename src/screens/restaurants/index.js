e186aa639343b95d158d9c70c99a7fc8dcf2dd9d
brandaoplaster <brandaoplaster@gmail.com>
2019-10-29 17:12:52 -0300

    Creating restaurant component

---
+++ src/screens/restaurants/index.js
@@ -0,0 +1,9 @@
+import React, { Fragament } from 'react';
+
+const Restaurants = () => (
+    <Fragament>
+        <b>Testing component Restaurants</b>
+    </Fragament>
+);
+
+export default Restaurants;
