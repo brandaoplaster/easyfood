af9f4cb1e9183be2e760a4e6b64944d797adf892
brandaoplaster <brandaoplaster@gmail.com>
2019-11-29 16:15:43 -0300

    Updated restaurant components

--- src/actions/restaturant.js
+++ src/actions/restaturant.js
@@ -1,10 +1,7 @@
+import restaurants from "../reducers/restaurants";
 import { LOAD_RESTAURANTS } from "./action_types";
-import api from "../services/api";
 
-export const loadReastaurants = () => async (dispatch) => {
-    let response = await api.loadRestaurants();
-    dispatch({
-        type: LOAD_RESTAURANTS,
-        restaurants: response.data.restaurants
-    })
-}
+export const loadRestaurants = () => ({
+    type: LOAD_RESTAURANTS,
+    restaurants: restaurants
+});
