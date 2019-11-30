4ab66135fb69c0f51bb069decbd6ba0c7c9e19c6
brandaoplaster <brandaoplaster@gmail.com>
2019-11-30 18:22:57 -0300

    Adpted to optionally receive a category parameter and call the API with test parameter

--- src/actions/restaturant.js
+++ src/actions/restaturant.js
@@ -1,7 +1,10 @@
-import restaurants from "../reducers/restaurants";
 import { LOAD_RESTAURANTS } from "./action_types";
+import api from "../services/api";
 
-export const loadRestaurants = () => ({
-    type: LOAD_RESTAURANTS,
-    restaurants: restaurants
-});
+export const loadRestaurants = (category = null) => async (dispatch) => {
+    let response = await api.loadRestaurants(category);
+    dispatch({
+       type: LOAD_RESTAURANTS,
+       restaurants: response.data.restaurants
+    });
+}
