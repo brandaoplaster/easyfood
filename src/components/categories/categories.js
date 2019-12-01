e8cc5c6ee1e4e172142fbe9f85e8c1ca611f4a39
brandaoplaster <brandaoplaster@gmail.com>
2019-12-01 20:53:43 -0300

    componentWillRender in it we are directly calling the loadCategories method of the API. We mapped the action creator loadRestaurants to filter restaurants by category when we click on any of them.

--- src/components/categories/categories.js
+++ src/components/categories/categories.js
@@ -1,21 +1,49 @@
-import React, {Component, Fragment} from 'react';
-import connect from "react-redux/es/connect/connect";
-import {bindActionCreators} from "redux";
-import {Box} from "rbx";
-import slickSettings from "./slick_settings";
+import React, { Fragment, Component } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Box } from "rbx";
 
+import "../../styles/categories.scss";
+import slickSettings from "./slick_settings";
+import api from "../../services/api";
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { loadRestaurants } from '../../actions/restaurant';
 
 class Categories extends Component{
     state = {
         categories: []
     }
 
+    filterByCategory = (category) => {
+        this.props.loadRestaurants(category);
+    }
+
+    componentWillMount() {
+        api.loadCategories().then(response => {
+           this.setState(() => ({ categories: response.data.categories }))
+        });
+    }
+
     render() {
         return (
             <Fragment>
+                <h3 className="title is-size-4">
+                    Categories
+                </h3>
                 <Box>
                     <Slider {...slickSettings}>
-
+                        {this.state.categories.map((category, i) => {
+                            return (
+                                <a href="#" onClick={() => this.filterByCategory(category)}>
+                                    <div className="slider-item" key={i}>
+                                        <img src={category.image_url} alt="new"/>
+                                        <span>{category.title}</span>
+                                    </div>
+                                </a>
+                            )
+                        })}
                     </Slider>
                 </Box>
             </Fragment>
