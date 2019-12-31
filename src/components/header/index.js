2eccd28fc31f52f016a8e8ef133cba07434c7097
brandaoplaster <brandaoplaster@gmail.com>
2019-12-31 19:13:36 -0300

    Updated Header component by adding showModal. Updated App.js and App.scss created style for model_root

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Navbar, Container } from 'rbx';
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { Navbar, Container, Icon } from 'rbx';
 import LogoImage from '../../assets/images/logo.png';
 import "../../styles/header.scss";
 import SearchBox from "../search_box_component";
+import { FaCrosshairs } from "react-icons/fa";
+import { showModal } from "../../actions/modal";
 
 const Header = () => (
     <div className="top-navbar">
@@ -16,10 +20,20 @@ const Header = () => (
                     <Navbar.Segment as="div" class="navbar-item navbar-center">
                         <SearchBox />
                     </Navbar.Segment>
+                    <Navbar.Segment as="div" align="end">
+                        <Navbar.Item onClick={() => this.props.showModal('ADDRESS_MODAL')}>
+                            <Icon color="has-custom-black">
+                                <FaCrosshairs />
+                            </Icon>
+                            <p>Endereço</p>
+                        </Navbar.Item>
+                    </Navbar.Segment>
                 </Navbar.Menu>
             </Navbar>
         </Container>
     </div>
 );
 
-export default Header;
+const mapDispatchToProps = dispatch => bindActionCreators({ showModal }, dispatch);
+
+export default connect(null, mapDispatchToProps)(Header);
