7cc8a37a10ad34638745b176e906bc5723694ea2
brandaoplaster <brandaoplaster@gmail.com>
2019-12-31 16:28:59 -0300

    Added call to endpoint getRestaurant in service api. And in show restaurant included component category products

--- src/screens/show_restaurant/index.js
+++ src/screens/show_restaurant/index.js
@@ -1,11 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import { Title, Box, Column, Image, Icon } from "rbx";
 import { FaStar } from "react-icons/fa";
+import api from "../../services/api";
+import CategoryProducts from "../../components/category_products";
 
 class ShowRestaurant extends Component {
     state = {
         restaurant: {}
     };
+
+    componentWillMount() {
+        api.getRestaurant(this.props.match.params.id).then(response => {
+            this.setState({restaurant: response.data.restaurant})
+        });
+    }
 
     render() {
         return <Fragment>
@@ -37,7 +45,7 @@ class ShowRestaurant extends Component {
             </Box>
             {
                 this.state.restaurant.product_categories && this.state.restaurant.product_categories.map((category, i)=> {
-                    // return <CategoryProducts {...category} key={i} />
+                    return <CategoryProducts {...category} key={i} />
                 })
             }
         </Fragment>
