7349aa656a988304143e7dfa9480d6388a443fb0
brandaoplaster <brandaoplaster@gmail.com>
2020-03-11 10:45:17 -0300

    Adjustments made to the OrderForm component in order to use the form and connect the component

--- src/components/order_form/index.js
+++ src/components/order_form/index.js
@@ -3,60 +3,101 @@ import { Box, Column, Title, Input, Field, Button, Control, Label } from 'rbx';
 import { connect } from 'react-redux';
 
 class OrderForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name:  '',
+      cpf:   '',
+      phone_number: '',
+      restaurant_id: this.props.restaurant.id
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
     render() {
+
         return (
             <Column.Group>
                 <Column size={10} offset={1}>
                     <Title size={5} className="has-text-custom-gray-darker">
                         Finalizar Pedido
                     </Title>
-
-                    <Box>
+                    <Box>         
+                
                         <Column.Group>
                             <Column size={10} offset={1}>
                                 <form>
                                     <Field>
                                         <Label>Nome</Label>
                                         <Control>
-                                            <Input type="text" placeholder="seu nome" name="name" />
+                                            <Input 
+                                                type="text" 
+                                                placeholder="Seu nome..."
+                                                name="name"
+                                                value={this.state.name}
+                                                onChange={this.handleInputChange}
+                                                required
+                                            />
                                         </Control>
                                     </Field>
 
                                     <Field>
                                         <Label>CPF</Label>
                                         <Control>
-                                            <Input type="text" placeholder="000.000.000-00" name="cpf" />
+                                            <Input 
+                                                type="text" 
+                                                placeholder='000.000.000-00'
+                                                name="cpf"
+                                                value={this.state.cpf}
+                                                onChange={this.handleInputChange}
+                                                required
+                                            />
                                         </Control>
                                     </Field>
-
                                     <Field>
                                         <Label>Telefone</Label>
                                         <Control>
-                                            <Input type="phone_number" name="phone" />
+                                            <Input 
+                                                type="text" 
+                                                placeholder='(00) 000000000'
+                                                name="phone_number"
+                                                value={this.state.phone_number}
+                                                onChange={this.handleInputChange}
+                                                required
+                                            />
                                         </Control>
                                     </Field>
-
+                            
                                     <Field>
-                                        <br />
+                                        <br/>
                                         <Title size={6} className="has-text-custom-gray-darker">
                                             Endereço de entrega
                                         </Title>
-                                        <span>
+                                        <p>
                                             {this.props.address.street}, {this.props.address.number}
-                                        </span>
-                                        <span>
+                                        </p>
+                                        <p>
                                             {this.props.address.city}, {this.props.address.state}
-                                        </span>
+                                        </p>
                                     </Field>
 
-                                    <br />
-
-                                    <Field kind="group" align="centered"> 
+                                    <br/>
+                                    <Field kind="group" align="centered">
                                         <Control>
                                             <Button size="medium" color="custom-orange">
-                                                <span className="has-text-white">
-                                                    Realizar Pedido
-                                                </span>
+                                                <span className="has-text-white">Realizar Pedido</span>
                                             </Button>
                                         </Control>
                                     </Field>
@@ -71,7 +112,8 @@ class OrderForm extends Component {
 }
 
 const mapStateToProps = store => ({
-    address: store.addressState.address
+  address: store.addressState.address,
+  restaurant: store.newOrderState.restaurant,
 });
 
 export default connect(mapStateToProps)(OrderForm);
