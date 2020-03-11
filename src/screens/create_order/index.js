94eaf48f7dc41650f39524ca713c1efc073334e7
brandaoplaster <brandaoplaster@gmail.com>
2020-03-11 09:11:24 -0300

    Creating component CreateOrder and including a router for it

---
+++ src/screens/create_order/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import { Column } from 'rbx';
+
+const CreateOrder = (props) => (
+    <Column.Group centered>
+        <Column size="4" offset="1">
+            Order Form
+        </Column>
+        <Column size="4" offset="1">
+            Product List
+        </Column>
+    </Column.Group>
+);
