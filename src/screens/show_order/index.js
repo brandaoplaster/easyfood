9bd498e4d111cce05929d6eecf971a3dee0ace73
brandaoplaster <brandaoplaster@gmail.com>
2020-03-13 00:06:44 -0300

    included in the Order Show screen the call to download the order and the connection to the store

--- src/screens/show_order/index.js
+++ src/screens/show_order/index.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import { Box, Column, Title } from 'rbx';
+import React, { Component, Fragment } from 'react';
+import { Box, Column, Title } from "rbx";
 import "../../styles/show_order.scss";
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { loadOrder } from "../../actions/order"; 
+
 class ShowOrder extends Component {
-    render() {
-        return (
-            <Column.Group className="status_box">
-                <Column size={4} offset={4} textAlign="centered">
-                    <Column.Group>
-                        <Column size={4} offset={2} textAlign="centered">
-                            <Box>
-                                <Title size={4} className="has-text-custom-green-darker status_msg">
-                                    Pedido a Caminho
+  componentWillMount() {
+    this.props.loadOrder(this.props.match.params.id);
+  }
+
+  render() {
+
+    return (
+        <Column.Group className="status_box">
+            <Column size={4} offset={4} textAlign="centered">
+                <Column.Group>
+                    <Column size={8} offset={2} textAlign="centered">
+                        <Box>         
+                            { 
+                                (this.props.order.status == 'waiting') ? (
+                                <Fragment>
+                                    <Title size={4} className="has-text-custom-green-darker status_msg">
+                                        Pedido a caminho
+                                    </Title>
+                                    <p className="status_description">
+                                        Em breve você recebera sua comida saudável em casa
+                                    </p>
+                                </Fragment>
+                                ) : (
+                                <Title size={4} className="has-text-custom-gray-darker status_msg">
+                                    Pedido entregue
                                 </Title>
-                                <p className="status_description">
-                                    Em breve você recebera sua comida saudável em casa
-                                </p>
-                            </Box>
-                        </Column>
-                    </Column.Group>
-                </Column>
-            </Column.Group>
-        )
-    }
+                                )
+                            }
+                        </Box>
+                    </Column>
+                </Column.Group>
+            </Column>
+        </Column.Group>
+    )
+  } 
 }
 
-export default ShowOrder;
+const mapStateToProps = store => ({
+  order: store.orderState.order
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({ loadOrder }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowOrder);
