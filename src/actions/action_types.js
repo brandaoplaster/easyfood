bc8704e87b033457ba4e66a484648108cfabce76
brandaoplaster <brandaoplaster@gmail.com>
2020-03-13 09:46:03 -0300

    Adding REMOVE_ORDER_ITEM action types and updated new_order

--- src/actions/action_types.js
+++ src/actions/action_types.js
@@ -3,4 +3,5 @@ export const SHOW_MODAL = 'SHOW_MODAL';
 export const HIDE_MODAL = 'HIDE_MODAL';
 export const SET_ADDRESS = 'SET_ADDRESS';
 export const ADD_ORER_ITEM = 'ADD_ORDER_ITEM';
+export const REMOVE_ORDER_ITEM = 'REMOVE_ORDER_ITEM';
 export const LOAD_ORDER = "LOAD_ORDER";
